37fe7d89a06799bdffc2b5013e7539d74e4bdeb4
Alon Dahari <climbinghobo@gmail.com>
2015-03-02 11:55:19 -0700

    add wishlist

--- main.js
+++ main.js
@@ -4,4 +4,19 @@ $(document).on('ready', function() {
   productsData.productsList.forEach(function(val){
   	$('#product-container').append(template(val))
   })
+
+  var wishlistScript = $('#wishlist-template').html()
+  var wishlistTemplate = Handlebars.compile(wishlistScript)
+
+  $('#wishlist-add').on('click', function(){
+  	var wishlistItem = $('#wishlist-input').val()
+  	if (wishlistItem) {
+  		$('#product-container').append(wishlistTemplate({title: wishlistItem}))
+
+  	}
+  })
+
+  $(document).on('click', '.wishlist-remove', function(){
+  	$(this).prev().andSelf().remove()
+  })
 });
